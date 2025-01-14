2e9ae5abec1029c0b45075386985cc675f6e3633
Maridzik <ibatullint7@gmail.com>
2025-01-14 16:54:43 +0500

    add active/inactive color

--- TimurTextInput.py
+++ TimurTextInput.py
@@ -13,25 +13,34 @@ font = pygame.font.Font(None, FONT_SIZE)
 
 
 class TextBox:
-    def __init__(self, x, y, width, height, max_length):
+    def __init__(self, x, y, width, height, max_length, active_color=(0, 0, 150), inactive_color=(200, 200, 200), text="", color_frame=(0, 0, 255), screen=None, visible = False):
         self.rect = pygame.Rect(x, y, width, height)
-        self.inactive_color = (200, 200, 200)
-        self.active_color = (0, 0, 150)
-        self.text = ""
+        self.inactive_color = inactive_color
+        self.active_color = active_color
+        self.text = text
         self.max_length = max_length
         self.active = False
+        self.screen = screen
+        self.visible = visible
+        if visible:
+            self.draw(screen)
+
     def draw(self, screen):
-        pygame.draw.rect(screen, (100, 100, 100), self.rect)
+        pygame.draw.rect(screen, self.inactive_color, self.rect)
         if self.active:
-            pygame.draw.rect(screen, self.active_color, self.rect, 2)
+            pygame.draw.rect(screen, self.active_color, self.rect, 3)
         else:
-            pygame.draw.rect(screen, self.inactive_color, self.rect, 2) 
+            pygame.draw.rect(screen, self.inactive_color, self.rect, 3) 
         text_surface = font.render(self.text, True, (255, 255, 255))
         screen.blit(text_surface, (self.rect.x + 5, self.rect.y + (self.rect.height - text_surface.get_height()) // 2))
 
     def input(self, event):
+        if not self.visible:
+            return
         if event.type == pygame.MOUSEBUTTONDOWN:
             self.active = self.rect.collidepoint(event.pos)
+            if not self.active:
+                return
         if event.type == pygame.KEYDOWN and self.active:
             if event.key == pygame.K_BACKSPACE:
                 if self.text:
@@ -40,31 +49,20 @@ class TextBox:
                 return self.text
             elif len(self.text) < self.max_length:
                 self.text += event.unicode
+        self.draw(self.screen)
 
-<<<<<<< HEAD
-'''
-Пример работы 
-=======
->>>>>>> b58ce955eaab9dd331fd728926ff750a6d6f3e76
 
-text_box = TextBox(200, 200, 400, 50, 20)
 
+text_box = TextBox(200, 200, 400, 50, 20, screen=screen, visible=True)
 running = True
-while running:
-    screen.fill((0, 0, 0))
+screen.fill((0, 0, 0))
+while running: 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         text_box.input(event)
-
-    text_box.draw(screen)
-
     
     pygame.display.flip()
 
-<<<<<<< HEAD
+
 pygame.quit()
-'''
-=======
-pygame.quit()
->>>>>>> b58ce955eaab9dd331fd728926ff750a6d6f3e76
