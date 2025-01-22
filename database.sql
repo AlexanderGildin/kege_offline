d995831fe4ebcba08eb57a6dc6f5718f954ab535
Ева <eva56m@yandex.ru>
2025-01-22 22:53:49 +0500

    added QtRedactor

--- database.sql
+++ database.sql
@@ -1,15 +1,15 @@
 --
--- Файл сгенерирован с помощью SQLiteStudio v3.4.12 в Сб дек 28 19:43:43 2024
+-- пїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ SQLiteStudio v3.4.12 пїЅ пїЅпїЅ пїЅпїЅпїЅ 28 19:43:43 2024
 --
--- Использованная кодировка текста: System
+-- пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ: System
 --
 PRAGMA foreign_keys = off;
 BEGIN TRANSACTION;
 
--- Таблица: Questions
-CREATE TABLE IF NOT EXISTS Questions (ID INTEGER PRIMARY KEY AUTOINCREMENT, number INTEGER, question BLOB, files TEXT, rows_in_answ INTEGER, col_in_answ INTEGER, answer TEXT, info TEXT, points TEXT);
+-- пїЅпїЅпїЅпїЅпїЅпїЅпїЅ: Questions
+CREATE TABLE IF NOT EXISTS Questions (ID INTEGER PRIMARY KEY AUTOINCREMENT, number TEXT, question BLOB, files TEXT, rows_in_answ INTEGER, col_in_answ INTEGER, answer TEXT, info TEXT, points TEXT);
 
--- Таблица: Variants
+-- пїЅпїЅпїЅпїЅпїЅпїЅпїЅ: Variants
 CREATE TABLE IF NOT EXISTS Variants (description TEXT, count_of_quest INTEGER, max_time_min INTEGER, internet_acsess INTEGER, secrkey_hash TEXT, info TEXT);
 
 COMMIT TRANSACTION;
