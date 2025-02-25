c12b9de111253f584238c360e074259db8485761
Mikhail <padalkomd@gmail.com>
2025-02-25 19:39:52 +0500

    Add files via upload

--- EvaDataBase.py
+++ EvaDataBase.py
@@ -1,20 +1,21 @@
-import re
 import sqlite3
-from zipfile import ZipFile
-import os
-import tempfile
+
 import pygame
-import shutil
 
 
 class DataBase:
-    def __init__(self, dbname='database', archive='archive.zip'):
-        with ZipFile(archive) as myzip:
-            myzip.extract(f"{dbname}.db", path='temp')
-        self.con = sqlite3.connect(f"temp/{dbname}.db")
+    def __init__(self, dbname='database.db'):
+        self.con = sqlite3.connect(dbname)
         self.cur = self.con.cursor()
 
     def create_tables(self):  # создает таблицы если их нет
+        with open('database.sql', 'r') as querytext:
+            queries = querytext.read().split('\n')
+        for query in queries:
+            self.cur.execute(query)
+        self.con.commit()
+
+    def create_tables_answ(self):
         with open('database.sql', 'r') as querytext:
             queries = querytext.read().split('\n')
         for query in queries:
@@ -39,15 +40,14 @@ class DataBase:
             self.cur.execute(f"""UPDATE Variants SET '{field}' = ?""", (value,))
         self.con.commit()
 
-    def quest_image(self, ID) -> pygame.image:  # получить картинку вопроса как объект pygame.image по ID вопроса
+    def quest_image(self, ID):  # получить картинку вопроса как объект pygame.image по ID вопроса
         filebytes = self.cur.execute(f"SELECT question FROM Questions WHERE ID = {ID}").fetchone()[0]
-        with open('temp/to_show_img.png', 'wb') as file:
+        with open('to_show_img.png', 'wb') as file:
             file.write(filebytes)
-        # ctypes.windll.kernel32.SetFileAttributesW('to_show_img.png', 2)
-        image = pygame.image.load('temp/to_show_img.png')
+        image = pygame.image.load('to_show_img.png')
         return image
 
-    def variant_info(self) -> dict:  # вся информация по варианту - возвращает словарь "поле": "значение"
+    def variant_info(self):  # вся информация по варианту - возвращает словарь "поле": "значение"
         keys = [i[0] for i in self.cur.execute("SELECT name FROM PRAGMA_TABLE_INFO('Variants')").fetchall()]
         values = self.cur.execute("SELECT * FROM Variants").fetchone()
         return {keys[i]: values[i] for i in range(len(keys))}
@@ -59,47 +59,6 @@ class DataBase:
         return self.cur.execute("""SELECT count_of_quest FROM Variants""").fetchone()[0]
 
     def get_file_names(self):
-        all = self.cur.execute("""SELECT files FROM Questions""").fetchall()
-        file_names = []
-        for i in all:
-            if i[0] is not None and i[0] != '':
-                file_names.append(re.split('[, ;]', i[0]))
-            else:
-                file_names.append([])
-        return file_names
-
-    def quest_id_by_num(self, quest_num):
-        if quest_num == 0:
-            id = self.cur.execute(f"""SELECT id FROM Questions WHERE number = 'Info'""").fetchone()[0]
-            return id
-        id = self.cur.execute(f"""SELECT id FROM Questions WHERE number = {quest_num}""").fetchone()[0]
-        return id
-
-    def clear_tables(self):
-        self.cur.execute("DELETE FROM Questions")
-        self.cur.execute("DELETE FROM Variants")
-        self.con.commit()
-
-    def get_rows_and_cols(self) -> dict:  # возвращает словать {номер вопроса: (строки, колонки)}
-        quests = self.cur.execute("""SELECT number, rows_in_answ, col_in_answ 
-        FROM Questions WHERE number != 'Info'""").fetchall()
-        return {int(quest[0]): (int(quest[1]), int(quest[2])) for quest in quests}
-
-    def get_hashed_password(self):
-        return self.cur.execute("""SELECT secrkey_hash FROM Variants""").fetchone()[0]
-
-
-def extract_and_move_file(
-        archive_path,
-        filename,
-        target_dir='.',
-        temp_dir='.'
-):
-    temp_dir = temp_dir
-
-    if archive_path.endswith('.zip'):
-        with ZipFile(archive_path, 'r') as zip_ref:
-            zip_ref.extract(filename, target_dir)
-    else:
-        raise ValueError("Неподдерживаемый формат архива")
-
+        names = [str(i[0]).strip() for i in
+                 self.cur.execute("SELECT files FROM Questions WHERE files IS NOT NULL").fetchall()]
+        return names
