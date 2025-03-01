198146cc37318d60a0af65f9b14026fb6112a5bf
Александр <sgildin@mail.ru>
2025-03-01 22:43:12 +0500

    v1.8 Добавлена многопоточность в станцию создания варианта Ошибки: станция тестирования не учитывается количество данных ответов окно перекрывается панелью задач не работает Shift Ins в полях ввода

--- Create_variant_station/QtRedactorThread.py
+++ Create_variant_station/QtRedactorThread.py
@@ -1,50 +1,45 @@
-import threading
-
 from PyQt6.QtWidgets import QApplication, QMainWindow, QScrollArea
+from PyQt6.QtCore import QThread, pyqtSignal
 from QtRedactorUI import Ui_MainWindow
 from EvaDataBaseCreate import DataBase
 from bcrypt import hashpw, gensalt
 from uniteImages import image_size_changes
 import os
+import time
 import shutil
 
+# https://habr.com/ru/companies/simbirsoft/articles/701020/
 
-class MainWindow(QMainWindow, Ui_MainWindow):
-    def __init__(self):
-        super().__init__()
-        self.setupUi(self)
-        self.setWindowTitle("QT Редактор вариантов")
+class insertVariant(QThread):
+    progress_signal = pyqtSignal(str)
+    finished_signal = pyqtSignal(str)
 
-        self.question_number_q = 1
-        self.insert_into_db.clicked.connect(self.funct_check_file)
-        self.add_quest.clicked.connect(self.add_question_funct)
-        try:
-            os.makedirs(os.getcwd() + r'\files')
-        except FileExistsError:
-            shutil.rmtree('files')
-            os.makedirs(os.getcwd() + r'\files')
+    def __init__(self, text):
+        QThread.__init__(self)
+        self.text = text
 
-    def insertVariant(self, er_log):
+    def run(self):
+        debug = False
         files = []
-        text = self.dataTextEdit.toPlainText().split('\n')
+        # text - список из строк окна описания варианта. Он передан в класс при создании его экземпляра
         secrkey_hash = ''
         max_time_min = ''  # там предполагается int
         internet_acsess = 0
 
         #  получение параметров варианта
         for i in range(8):
-            if text[i].startswith('COUNT_OF_QUESTIONS'):
-                count_of_quest = int(text[i].split('=')[1])
-            if text[i].startswith('MAXTIME'):
-                if len(text[1].split('=')[1]) > 0 and text[1].split('=')[1] != '0':
-                    max_time_min = int(text[1].split('=')[1])
-            if text[i].startswith('KEY'):
-                if len(text[2].split('=')[1]) > 0:
-                    secrkey_hash = hashpw(bytes(text[2].split('=')[1], 'utf-8'), salt=gensalt())
-            if text[i].startswith('DESCRIPTION'):
-                description = text[i].split('=')[1]
-            if text[i].startswith('INTERNET_ACCESS'):
-                if text[i].split('=')[1] == '1':
+            if self.text[i].startswith('COUNT_OF_QUESTIONS'):
+                count_of_quest = int(self.text[i].split('=')[1])
+            if self.text[i].startswith('MAXTIME'):
+                if len(self.text[1].split('=')[1]) > 0 and self.text[1].split('=')[1] != '0':
+                    max_time_min = int(self.text[1].split('=')[1])
+            if self.text[i].startswith('KEY'):
+                if len(self.text[2].split('=')[1]) > 0:
+                    secrkey_hash = hashpw(bytes(self.text[2].split('=')[1], 'utf-8'), salt=gensalt())
+            if self.text[i].startswith('DESCRIPTION'):
+                description = self.text[i].split('=')[1]
+            if self.text[i].startswith('INTERNET_ACCESS'):
+                if self.text[i].split('=')[1] == '1':
                     internet_acsess = 1
         database = DataBase(description + '.db')
         DB_with_ans = DataBase(f"{description}_ans.db")
@@ -73,32 +68,36 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
         #  количество строк по варианту фиксировано, поэтому начинаю сразу со строк по вопросам
         i = 4
+        if debug: print(self.text)
         try:
-            for i in range(5, len(text)):
+            for i in range(4, len(self.text)):
+                if debug: print("очередная строка ", i)
                 #  если начался новый вопрос/информация, записываются в бд и сбрасываются в переменной собранные данные
-                if text[i].startswith('I ') or text[i].startswith('Q'):
+                if self.text[i].startswith('I ') or self.text[i].startswith('Q'):
+                    if debug: print("отработка строки 75", self.text[i], i)
                     if len(quest_data) > 0:
                         if quest_data[0][1] != "Info":
                             database.new_quest(quest_data)
                             DB_with_ans.new_quest(ans_data)
                         else:
                             database.new_quest(quest_data)
-                    er_log.setPlainText(f"Формирование вопроса {text[i]}")
-                    if text[i].startswith('I '):
+                    if self.text[i].startswith('I '):
                         quest_data = [("number", "Info")]
                     else:
-                        quest_data = [("number", int(text[i].split()[0][1:]))]
-                        ans_data = [("number", int(text[i].split()[0][1:]))]
-                    required_width = int(text[i].split(' ')[1])
+                        quest_data = [("number", int(self.text[i].split()[0][1:]))]
+                        ans_data = [("number", int(self.text[i].split()[0][1:]))]
+                    required_width = int(self.text[i].split(' ')[1])
+                    self.progress_signal.emit((self.text[i].split()[0][1:]))
+                    if debug: print((self.text[i].split()[0][1:]))
+                    # if debug: time.sleep(10)
                     continue
-                param = text[i].split('=')
-
+                param = self.text[i].split('=')
                 #  обработка возможных полей и добавление их значений в quest_data
                 if param[0] == 'IMG':
                     lname = param[1].replace(' ', '')
                     lname = lname.replace(';', ',')
-                    image_size_changes(lname.split(','), required_width, result_file_name='questImg.png')
-                    with open('questImg.png', 'rb') as image:
+                    image_size_changes(lname.split(','), required_width, result_file_name='../questImg.png')
+                    with open('../questImg.png', 'rb') as image:
                         quest_bytes = image.read()
                     quest_data.append(("question", quest_bytes))
 
@@ -121,27 +120,47 @@ class MainWindow(QMainWindow, Ui_MainWindow):
                     ans_data.append(("info", f"separator='{param[1]}'"))
                 elif param[0] == 'ANSW':
                     ans_data.append(("answer", param[1]))
+                if debug: print("отработка строки 121")
             #  запись последнего невошедшего вопроса
             database.new_quest(quest_data)
             DB_with_ans.new_quest(ans_data)
-        except:
+        except Exception as ex:
+            if debug: print("сработала ошибка в строке 127", str(ex))
+            self.progress_signal.emit(str(ex))
             pass
 
         database.close()
         files.append(f'{description}.db')
         for x in files:
             shutil.copy(x, str(os.getcwd() + r'\files' + "\\" + x))  # ЗАМЕНИТЬ НА MOVE
-        shutil.make_archive(f'{description}', 'zip', root_dir='files')  # Похоже, что создает даже если такой был уже
+        shutil.make_archive(f'{description}', 'zip', root_dir='../files')  # Похоже, что создает даже если такой был уже
         try:
             os.rename(f'{description}.zip', f'{description}.tsk')
         except:
             os.remove(f'{description}.tsk')
             os.rename(f'{description}.zip', f'{description}.tsk')
-        self.logsTextEdit.setPlainText(
-            f'Создан файл КИМ: {description}.tsk, поместите его в станцию тестирования. Создан файл: '
-            f'{description}_ans.db, поместите его в станцию проверки.')
-        shutil.rmtree('files')
-        self.Mode = "Файл tsk создан"
+
+        self.finished_signal.emit(description)
+
+        shutil.rmtree('../files')
+
+
+class MainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.setWindowTitle("QT Редактор вариантов")
+
+        self.question_number_q = 1
+        self.insert_into_db.clicked.connect(self.funct_check_file)
+        self.add_quest.clicked.connect(self.add_question_funct)
+        try:
+            os.makedirs(os.getcwd() + r'\files')
+        except FileExistsError:
+            shutil.rmtree('../files')
+            os.makedirs(os.getcwd() + r'\files')
+
+
 
     def funct_check_file(self):
         flag_img = False
@@ -155,7 +174,6 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         error_output = []
         number_of_questions = 1
         text = self.dataTextEdit.toPlainText().split('\n')
-        self.Mode = "Проверка синтаксиса"
 
         try:
             for i in range(4):
@@ -267,7 +285,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
                         #  raise NoResponse
                     else:
                         answer_list.append(self.param[1].split(separator))
-            self.logsTextEdit.setPlainText('ОК')
+            self.logsTextEdit.setPlainText('Дождитесь окончания формирования варианта.')
         except Exception:
             error_output.append('Неизвестная ошибка в вопросе Q' + str(self.q_num))
         if int(self.q_num) != int(self.count_of_question):
@@ -278,13 +296,25 @@ class MainWindow(QMainWindow, Ui_MainWindow):
                 break
         if len(error_output) > 0:
             self.logsTextEdit.setPlainText('\n'.join(error_output))
-        if len(error_output) == 0: #ПРАВКА А.Г.
-            self.Mode = "Создание файла tsk в отдельном потоке"
-            self.my_Thread = threading.Thread(target=self.insertVariant, args=(self.logsTextEdit,))
-            self.insert_into_db.hide()
-            self.my_Thread.start()
-            self.insert_into_db.show()
-            # self.insertVariant()
+        if len(error_output) == 0:
+            self.myinsertObject = insertVariant(self.dataTextEdit.toPlainText().split('\n'))
+            self.myinsertObject.progress_signal.connect(self.updatelog)
+            self.myinsertObject.finished_signal.connect(self.unblock)
+            self.insert_into_db.setEnabled(False)
+            self.dataTextEdit.setEnabled(False)
+            self.add_quest.setEnabled(False)
+            self.myinsertObject.start()
+
+    def updatelog(self, x):
+        self.logsTextEdit.setPlainText(f"Обработка вопроса: {x}")
+
+    def unblock(self, file_name):
+        self.logsTextEdit.setPlainText(
+            f'Создан файл КИМ: {file_name}.tsk, поместите его в станцию тестирования.\nСоздан файл: '
+            f'{file_name}_ans.db, поместите его в станцию проверки.')
+        self.insert_into_db.setEnabled(True)
+        self.dataTextEdit.setEnabled(True)
+        self.add_quest.setEnabled(True)
 
     def add_question_funct(self):
         self.question_number_q = int(self.question_number_q) + 1
